461b094c117e2999b2edbfbc5387707880b9f23b
RonRud <31389934+RonRud@users.noreply.github.com>
2022-03-24 12:33:42 +0200

    Added system console write line to writeHello

--- hookInto/writeHello.cpp
+++ hookInto/writeHello.cpp
@@ -2,11 +2,13 @@
 #include <fstream>
 #include <string>
 #include <windows.h>
+#include <stdlib.h>
 
 int main() {
     std::string input;
     std::cin >> input;
 
+    system("echo Let's see if you can read this\n");
     //LPSTR x = TEXT("shnitzel");
     //LPSTR y = L"shnitzel";
     if(0 == lstrcmpA("shnitzel", "shnitzel")) {std::cout << "wha a shnitzel" << std::endl;}
